c28c0b479f773d582ce2690fa37d2b99a718a165
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-25 22:54:35 +0530

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed("h3", {
+    strings: [
+        "Java Full Stack Developer",
+        "Frontend Developer",
+        "Web Developer"
+    ],
+    typeSpeed: 40,
+    backSpeed: 30,
+    backDelay: 50,
+    loop: true
+});
