0a01807231f5e94f187a3e4c7ef02825ffc8b209
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-27 15:42:15 +0530

    change all things

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".multiple-text", {
+
+    strings:[
+        "Java Developer",
+        "Web Developer",
+        "Software Developer"
+    ],
+
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
